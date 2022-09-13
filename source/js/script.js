2fbe934a35128d0e62d19cb7d16e7644ac116475
Aleksey Yakovlev <volkracing555@gmail.com>
2022-09-13 16:41:51 +0300

    Финалочка №6

--- source/js/script.js
+++ source/js/script.js
@@ -3,10 +3,10 @@ let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.classList.remove("main-nav__toggle--hidden");
-navMain.classList.add("main-nav--closed");
+navToggle.classList.remove('main-nav__toggle--hidden');
+navMain.classList.add('main-nav--closed');
 
-navToggle.addEventListener("click", function () {
-  navMain.classList.toggle("main-nav--opened");
-  navMain.classList.toggle("main-nav--closed");
+navToggle.addEventListener('click', function () {
+  navMain.classList.toggle('main-nav--opened');
+  navMain.classList.toggle('main-nav--closed');
 })
